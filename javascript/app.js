acf6b3bec5ef8610e242f9374661f31ab1bc9492
IfraA <ifraa.aijaz@gmail.com>
2018-02-28 21:27:10 -0800

    modified animate btn

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,5 @@
 //cerate an array of the topics for each button
 
-// $(document).ready(function() {
 var topics = ["shrug", "pout", "confused", "excited"];
 //function to display all gif buttons
 
@@ -11,19 +10,19 @@ function displayButtons() {
   // Looping through the array of topics
   for (var i = 0; i < topics.length; i++) {
     // Then dynamicaly generating buttons for each topic in the array
-    var g = $("<button>");
+    var gifButton = $("<button>");
     // Adding a class
-    g.addClass("gif");
+    gifButton.addClass("gif");
     // Added a data-attribute
-    g.attr("data-name", topics[i]);
+    gifButton.attr("data-name", topics[i]);
     // Provided the initial button text
-    g.text(topics[i]);
+    gifButton.text(topics[i]);
     // Added the button to the HTML
-    $("#buttons-view").append(g);
-    console.log(g);
+    $("#buttons-view").append(gifButton);
+    // console.log(gifButton);
   }
 }
-
+displayButtons();
 //create an on click event to add gif
 $("#add-gif").on("click", function(event) {
   event.preventDefault();
@@ -37,15 +36,16 @@ $("#add-gif").on("click", function(event) {
   displayButtons();
 });
 //function to display giphys on clikcing the Gif Button
-$("g").on("click", function() {
+$(document).on("click", "button", function() {
   //grabbing the data-name attribute and storing it in the gifbutton
   var gifButton = $(this).attr("data-name");
   var queryURL =
-    "http://api.giphy.com/v1/gifs/search?q=" +
+    "https://api.giphy.com/v1/gifs/search?q=" +
     gifButton +
-    "&api_key=YSbMcpktYF3fStJ6nmvWXH3xc7X0jRSv4&limit=10";
+    "&api_key=SbMcpktYF3fStJ6nmvWXH3xc7X0jRSv4&limit=10";
+  //ajax call
   $.ajax({
-    url: queryUrl,
+    url: queryURL,
     method: "GET",
   })
     //after response comes from ajax
@@ -58,7 +58,7 @@ $("g").on("click", function() {
       // Looping through each result item
       for (var i = 0; i < results.length; i++) {
         // Creating and storing a div tag
-        var topicsDiv = $("<div>");
+        var topicDiv = $("<div>");
 
         // Creating a paragraph tag with the result item's rating
         var p = $("<p>").text("Rating: " + results[i].rating);
@@ -67,13 +67,29 @@ $("g").on("click", function() {
         var topicImage = $("<img>");
         // Setting the src attribute of the image to a property pulled off the result item
         topicImage.attr("src", results[i].images.fixed_height_still.url);
-        //
+        //storing urls in variables called data-still and data-animate
+        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+        topicImage.attr("data-animate", results[i].images.fixed_height.url);
+        topicImage.attr("data-state", "still");
 
-        // Appending the paragraph and image tag to the animalDiv
-        topcDiv.append(p);
+        // Appending the paragraph and image tag to the topicDiv
+        topicDiv.append(p);
         topicDiv.append(topicImage);
         $("#gifs-appear-here").prepend(topicDiv);
       }
     });
 });
 //create a function for animatestill
+$(document).on("click", "img", function() {
+  var dataState = $(this).attr("data-state");
+  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  // Then, set the image's data-state to animate
+  // Else set src to the data-still value
+  if (dataState === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
